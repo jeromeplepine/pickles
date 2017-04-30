a6947f4a94fa120c0b18c0ff6cac8ceb0cd537b0
jeromeplepine <jeromeplepine@gmail.com>
2017-04-30 00:32:19 -0400

    gpio testing utility code

--- gpio_testing.py
+++ gpio_testing.py
@@ -1,9 +1,27 @@
-print "this a test running, make sure to GPIO.cleanup() afterwards"
+
 
 sudo python
 import RPi.GPIO as GPIO
+
+GPIO.cleanup()
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-GPIO.output(18, GPIO.HIGH)
-pwm = GPIO.PWM(18, 1000)
-pwm.start(50)
+print "BCM"
+print "this a test running, make sure to GPIO.cleanup() afterwards"
+
+def config_out(x):
+	GPIO.setup(x, GPIO.OUT)
+
+
+def output_high():
+	GPIO.output(x, GPIO.HIGH)
+	print"HIgh output on selected BCM pin"
+
+def output_low():
+	GPIO.output(x, GPIO.LOW)
+	print"HIgh output on selected BCM pin"
+
+
+def clean():
+	GPIO.cleanup()
+	print"Floor is clean"
